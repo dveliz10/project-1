1a568655ac69e740f2bbb7d847d0846cada90042
dveliz10 <70672340+dveliz10@users.noreply.github.com>
2020-10-07 20:09:53 -0400

    Update project-2.js

--- project-2.js
+++ project-2.js
@@ -1,14 +1,22 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-  
+  <meta charset="UTF-8">
     <script type="text/javascipt">
     
-  document.addEventListener('DOMContentListner', function() {
-    document.querySelector("button").onclick = inc;
+  document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector("button").onclick = function() {
+      let msg = document.querySelector("#message").value;
+      alert(msg);
+    }
+    })
+                      
       </script>
       </head>
       <body>
-      
+      <form id="form">
+        <input id="message" type="text"/>
+          <input type="submit">
+            </form>
       </body>
   </html>
